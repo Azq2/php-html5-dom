1de8b42583ca2076856812dd20513b2140f21bb1
Azq2 <kirill.zhumarin@gmail.com>
2020-01-20 01:06:21 +0200

    node getters

--- src/node.c
+++ src/node.c
@@ -1,9 +1,17 @@
 #include "node.h"
 #include "zend_exceptions.h"
+#include "zend_smart_str.h"
 #include "parser.h"
 #include "ext/spl/spl_exceptions.h"
+
 #include "lexbor/html/html.h"
+#include "lexbor/html/interfaces/element.h"
+
 #include "lexbor/html/interface.h"
+#include "lexbor/dom/interfaces/element.h"
+#include "lexbor/dom/interfaces/document_type.h"
+#include "lexbor/dom/interfaces/character_data.h"
+#include "lexbor/dom/interfaces/processing_instruction.h"
 
 /*
  * Methods
@@ -230,9 +238,23 @@ PHP_METHOD(HTML5_DOM_DocumentNonStandart, getParseErrors) {
 
 /* HTML5\DOM\Node */
 int html5_dom_node__nodeType(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_node_t *self = (lxb_dom_node_t *) obj->ptr;
+	if (!write)
+		ZVAL_LONG(val, self->type);
 	return 0;
 }
 int html5_dom_node__nodeName(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_node_t *self = (lxb_dom_node_t *) obj->ptr;
+	if (!write) {
+		size_t str_len = 0;
+		const lxb_char_t *str = lxb_dom_node_name(self, &str_len);
+		if (str) {
+			ZVAL_STRINGL(val, str, str_len);
+		} else {
+			ZVAL_NULL(val);
+		}
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_node__baseURI(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
@@ -338,27 +360,58 @@ int html5_dom_attr__ownerElement(html5_dom_object_wrap_t *obj, zval *val, int wr
 
 /* HTML5\DOM\Document */
 int html5_dom_document__URL(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	if (!write) {
+		ZVAL_NULL(val);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_document__documentURI(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	if (!write) {
+		ZVAL_NULL(val);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_document__origin(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	if (!write) {
+		ZVAL_NULL(val);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_document__compatMode(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_document_t *self = (lxb_dom_document_t *) obj->ptr;
+	if (!write) {
+		if (self->compat_mode == LXB_DOM_DOCUMENT_CMODE_QUIRKS) {
+			// if the document is in quirks mode.
+			ZVAL_STRINGL(val, "BackCompat", sizeof("BackCompat") - 1);
+		} else {
+			// if the document is in no-quirks (also known as "standards") mode or limited-quirks (also known as "almost standards") mode.
+			ZVAL_STRINGL(val, "CSS1Compat", sizeof("CSS1Compat") - 1);
+		}
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_document__characterSet(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	if (!write) {
+		ZVAL_STRINGL(val, "UTF-8", sizeof("UTF-8") - 1);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_document__charset(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
-	return 0;
+	return html5_dom_document__characterSet(obj, val, write, debug);
 }
 int html5_dom_document__inputEncoding(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
-	return 0;
+	return html5_dom_document__characterSet(obj, val, write, debug);
 }
 int html5_dom_document__contentType(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	if (!write) {
+		ZVAL_STRINGL(val, "text/html", sizeof("text/html") - 1);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_document__doctype(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
@@ -380,50 +433,180 @@ int html5_dom_document__documentElement(html5_dom_object_wrap_t *obj, zval *val,
 
 /* HTML5\DOM\DocumentType */
 int html5_dom_documenttype__name(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_document_type_t *self = (lxb_dom_document_type_t *) obj->ptr;
+	if (!write) {
+		ZVAL_STRINGL(val, self->name.data, self->name.length);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_documenttype__publicId(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_document_type_t *self = (lxb_dom_document_type_t *) obj->ptr;
+	if (!write) {
+		ZVAL_STRINGL(val, self->public_id.data, self->public_id.length);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_documenttype__systemId(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_document_type_t *self = (lxb_dom_document_type_t *) obj->ptr;
+	if (!write) {
+		ZVAL_STRINGL(val, self->system_id.data, self->system_id.length);
+		return 1;
+	}
 	return 0;
 }
 
 /* HTML5\DOM\CharacterData */
 int html5_dom_characterdata__data(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_character_data_t *self = (lxb_dom_character_data_t *) obj->ptr;
+	if (write) {
+		// TODO: set
+	} else {
+		ZVAL_STRINGL(val, self->data.data, self->data.length);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_characterdata__length(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_character_data_t *self = (lxb_dom_character_data_t *) obj->ptr;
+	if (!write) {
+		ZVAL_LONG(val, self->data.length);
+		return 1;
+	}
 	return 0;
 }
 
 /* HTML5\DOM\Text */
 int html5_dom_text__wholeText(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_text_t *self = (lxb_dom_text_t *) obj->ptr;
+	if (!write) {
+		// Find first whole text node
+		lxb_dom_node_t *node = lxb_dom_interface_node(self);
+		while (node->prev && node->prev->type == LXB_DOM_NODE_TYPE_TEXT)
+			node = node->prev;
+		
+		// Concat all whole text nodes
+		smart_str buf = {0};
+		while (node && node->type == LXB_DOM_NODE_TYPE_TEXT) {
+			lxb_dom_character_data_t *node_char_data = lxb_dom_interface_character_data(node);
+			if (node_char_data->data.length)
+				smart_str_appendl(&buf, node_char_data->data.data, node_char_data->data.length);
+			node = node->next;
+		}
+		
+		// Return result string
+		if (buf.s) {
+			ZSTR_VAL(buf.s)[ZSTR_LEN(buf.s)] = '\0';
+			ZVAL_NEW_STR(val, buf.s);
+		} else {
+			ZVAL_EMPTY_STRING(val);
+		}
+		
+		return 1;
+	}
 	return 0;
 }
 
 /* HTML5\DOM\ProcessingInstruction */
 int html5_dom_processinginstruction__target(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_processing_instruction_t *self = (lxb_dom_processing_instruction_t *) obj->ptr;
+	if (!write) {
+		ZVAL_STRINGL(val, self->target.data, self->target.length);
+		return 1;
+	}
 	return 0;
 }
 
 /* HTML5\DOM\Element */
 int html5_dom_element__namespaceURI(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_element_t *self = (lxb_dom_element_t *) obj->ptr;
+	if (!write) {
+		lxb_dom_document_t *dom_document = lxb_dom_interface_node(self)->owner_document;
+		size_t str_len = 0;
+		const lxb_char_t *str = lxb_ns_by_id(dom_document->ns, lxb_dom_interface_node(self)->ns, &str_len);
+		if (str) {
+			ZVAL_STRINGL(val, str, str_len);
+		} else {
+			ZVAL_NULL(val);
+		}
+		return 1;
+	}
 	return 0;
 }
+
 int html5_dom_element__prefix(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_element_t *self = (lxb_dom_element_t *) obj->ptr;
+	if (!write) {
+		size_t str_len = 0;
+		const lxb_char_t *str = lxb_dom_element_prefix(self, &str_len);
+		if (str) {
+			ZVAL_STRINGL(val, str, str_len);
+		} else {
+			ZVAL_NULL(val);
+		}
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_element__localName(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_element_t *self = (lxb_dom_element_t *) obj->ptr;
+	if (!write) {
+		size_t str_len = 0;
+		const lxb_char_t *str = lxb_dom_element_local_name(self, &str_len);
+		if (str) {
+			ZVAL_STRINGL(val, str, str_len);
+		} else {
+			ZVAL_NULL(val);
+		}
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_element__tagName(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_element_t *self = (lxb_dom_element_t *) obj->ptr;
+	if (!write) {
+		size_t str_len = 0;
+		const lxb_char_t *str = lxb_dom_element_tag_name(self, &str_len);
+		if (str) {
+			ZVAL_STRINGL(val, str, str_len);
+		} else {
+			ZVAL_NULL(val);
+		}
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_element__id(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_element_t *self = (lxb_dom_element_t *) obj->ptr;
+	if (write) {
+		// TODO: write
+	} else {
+		size_t str_len = 0;
+		const lxb_char_t *str = lxb_dom_element_id(self, &str_len);
+		if (str) {
+			ZVAL_STRINGL(val, str, str_len);
+		} else {
+			ZVAL_EMPTY_STRING(val);
+		}
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_element__className(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_element_t *self = (lxb_dom_element_t *) obj->ptr;
+	if (write) {
+		// TODO: write
+	} else {
+		size_t str_len = 0;
+		const lxb_char_t *str = lxb_dom_element_class(self, &str_len);
+		if (str) {
+			ZVAL_STRINGL(val, str, str_len);
+		} else {
+			ZVAL_EMPTY_STRING(val);
+		}
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_element__classList(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
@@ -444,20 +627,64 @@ int html5_dom_parentnode__children(html5_dom_object_wrap_t *obj, zval *val, int 
 	return 0;
 }
 int html5_dom_parentnode__firstElementChild(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_node_t *self = (lxb_dom_node_t *) obj->ptr;
+	if (!write) {
+		lxb_dom_node_t *node = self->first_child;
+		while (node && node->type != LXB_DOM_NODE_TYPE_ELEMENT)
+			node = node->next;
+		html5_dom_node_to_zval(node, val);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_parentnode__lastElementChild(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_node_t *self = (lxb_dom_node_t *) obj->ptr;
+	if (!write) {
+		lxb_dom_node_t *node = self->last_child;
+		while (node && node->type != LXB_DOM_NODE_TYPE_ELEMENT)
+			node = node->prev;
+		html5_dom_node_to_zval(node, val);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_parentnode__childElementCount(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_node_t *self = (lxb_dom_node_t *) obj->ptr;
+	if (!write) {
+		zend_long count = 0;
+		lxb_dom_node_t *node = self->first_child;
+		while (node) {
+			if (node->type == LXB_DOM_NODE_TYPE_ELEMENT)
+				++count;
+			node = node->next;
+		}
+		ZVAL_LONG(val, count);
+		return 1;
+	}
 	return 0;
 }
 
 /* HTML5\DOM\NonDocumentTypeChildNode */
 int html5_dom_nondocumenttypechildnode__previousElementSibling(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_node_t *self = (lxb_dom_node_t *) obj->ptr;
+	if (!write) {
+		lxb_dom_node_t *node = self->next;
+		while (node && node->type != LXB_DOM_NODE_TYPE_ELEMENT)
+			node = node->next;
+		html5_dom_node_to_zval(node, val);
+		return 1;
+	}
 	return 0;
 }
 int html5_dom_nondocumenttypechildnode__nextElementSibling(html5_dom_object_wrap_t *obj, zval *val, int write, int debug) {
+	lxb_dom_node_t *self = (lxb_dom_node_t *) obj->ptr;
+	if (!write) {
+		lxb_dom_node_t *node = self->prev;
+		while (node && node->type != LXB_DOM_NODE_TYPE_ELEMENT)
+			node = node->prev;
+		html5_dom_node_to_zval(node, val);
+		return 1;
+	}
 	return 0;
 }
 
@@ -481,14 +708,14 @@ void html5_dom_node_to_zval(lxb_dom_node_t *node, zval *retval) {
 	
 	html5_dom_object_wrap_t *intern = (html5_dom_object_wrap_t *) node->user;
 	
-	// If Node already have PHP representation - increment ref count
+	// Reuse exists php object
 	if (intern) {
 		GC_ADDREF(&intern->std);
 		ZVAL_OBJ(retval, &intern->std);
 		return;
 	}
 	
-	// Create new Node representation in PHP
+	// Create new php object
 	zend_class_entry *ce = html5_dom_node_ce;
 	switch (node->type) {
 		case LXB_DOM_NODE_TYPE_ELEMENT:
